23676aeaf98076aa9d52e50137514a8f6c06569d
Yunus MAKAN <makanyunus@gmail.com>
2020-10-12 15:29:16 +0300

    Initial Commit

---
+++ PiCKER(GALERiYi ACMAK)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PiCKER(GALERiYi ACMAK)
+//
+//  Created by MAKAN on 12.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
